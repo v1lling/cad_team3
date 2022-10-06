0789800774dd728896388542f4ce083655e6b730
v1lling <sascha.villing@web.de>
2022-10-06 19:01:48 +0200

    Add default nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <v-app>
+    <v-app-bar dense dark app fixed >
+      <v-toolbar-title class="v-title">Team 3</v-toolbar-title>
+
+      <v-toolbar-items>
+        <v-btn to="/">
+          Home
+        </v-btn>
+        <v-btn to="/about">
+          About
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
     <v-main>
       <router-view/>
     </v-main>
